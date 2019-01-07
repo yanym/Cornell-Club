35670f86a0714df81deaa21474acd7c5cf8ba385
Yiming <ymyan6@gmail.com>
2019-01-07 01:19:24 -0800

    css: row 1020->1100

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,133 +1,27 @@
 import React, { Component } from "react";
 // import PhotoGrid from "react-photo-feed";
-import { Grid, Row, Col, Button, Thumbnail, Image } from 'react-bootstrap';
-import Gallery from 'react-grid-gallery';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 // import "react-photo-feed/library/style.css";
 
 export default class photos extends Component {
     render() {
-        const IMAGES =
-        [{
-                src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-                thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-                thumbnailWidth: 320,
-                thumbnailHeight: 500,
-                caption: "After Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)"
-        },
-        {
-                src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-                thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-                thumbnailWidth: 320,
-                thumbnailHeight: 500,
-                // tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-                caption: "Boats (Jeshu John - designerspicAfter Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)s.com)"
-        },
-        
-        {
-                src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-                thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-                thumbnailWidth: 320,
-                thumbnailHeight: 500,
-                caption: "Boats (Jeshu John - designerspicAfter Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)After Rain (Jeshu John - designerspics.com)s.com)"
-        }]
-
         return (
             <div style={{background: '#ebeeee', display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                <div>
-                    <Row>
-                        <Col xs={8} sm={4}>
-                        <Image src="images/128.png"/>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={6} md={4}>
+                           12323123
                         </Col>
-                        <Col xs={8} sm={4}>
-                        <Image src="images/128.png" />
+                        <Col xs={6} md={4}>
+                            3123123123
                         </Col>
-                        <Col xs={8} sm={4}>
-                        <Image src="images/128.png" />
+                        <Col xs={6} md={4}>
+                            312312312
                         </Col>
                     </Row>
-                </div>
-                <Gallery images={IMAGES}/>
+                </Grid>
             </div>
-
-
-
-
-
-            // <Grid>
-            // <Row>
-            //     <Col xs={6} md={4}>
-            //     <Thumbnail src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt="150x150" circle responsive>
-            //         <h3>Thumbnail label</h3>
-            //         <p>Description</p>
-            //         {/* <p>
-            //         <Button bsStyle="primary">Button</Button>
-            //         &nbsp;
-            //         <Button bsStyle="default">Button</Button>
-            //         </p> */}
-            //     </Thumbnail>
-            //     </Col>
-            //     <Col xs={6} md={4}>
-            //     <Thumbnail src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt="150x150" circle responsive>
-            //         <h3>Thumbnail label</h3>
-            //         <p>Description</p>
-            //         {/* <p>
-            //         <Button bsStyle="primary">Button</Button>
-            //         &nbsp;
-            //         <Button bsStyle="default">Button</Button>
-            //         </p> */}
-            //     </Thumbnail>
-            //     </Col>
-            //     <Col xs={6} md={4}>
-            //     <Thumbnail src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt="150x150" circle responsive>
-            //         <h3>Thumbnail label</h3>
-            //         <p>Description</p>
-            //         {/* <p>
-            //         <Button bsStyle="primary">Button</Button>
-            //         &nbsp;
-            //         <Button bsStyle="default">Button</Button>
-            //         </p> */}
-            //     </Thumbnail>
-            //     </Col>
-            // </Row>
-            // </Grid>
-        );
+        )
     }
-    // render() {
-    //     const P = [
-    //         {
-    //             id : 1, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
-    //         },
-    //         {
-    //             id : 2, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
-    //         },
-    //         {
-    //             id : 3, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
-    //         },
-    //         {
-    //             id : 4, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
-    //         },
-    //         {
-    //             id : 5, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
-    //         },
-    //         {
-    //             id : 6, src : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4240/35527849422_25a0a67df6_b.jpg"
-    //         },
-    //         {
-    //             id : 7, src : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_n.jpg",
-    //             bigSrc : "https://farm5.staticflickr.com/4077/34824083444_f5f050e31c_b.jpg"
-    //         }
-    //     ];
-    //     return (
-    //         <div>
-    //             <PhotoGrid columns={3} photos={P} />
-    //         </div>
-    //     );
-    // }
 }
